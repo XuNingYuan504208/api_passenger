77053fe2a1243adb02e7cc421cd034d24fcbdef5
ny <hrbuswxuli@163.com>
2021-03-14 22:41:20 +0800

    api_passenger commit

---
+++ src/test/java/com/ny/api_passenger/ApiPassengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ny.api_passenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPassengerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
